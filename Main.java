9a09524cc159b8c53e2ee1eb46fb204534e1ab49
marifkur <work.marifk@gmail.com>
2019-03-04 16:57:48 +0700

    tambahan file

---
+++ Main.java
@@ -0,0 +1,39 @@
+
+/**
+ * Write a description of class Main here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Main
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Main
+     */
+    public Main(int xx)
+    {
+        // initialise instance variables
+        x = xx;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+    
+    public static void main (String[] args){
+        Main m=new Main(5);
+        int o=m.sampleMethod(5);
+        System.out.println(o);
+    }
+}
